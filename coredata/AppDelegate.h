7027db2ea0831fc1df904c742cb021e6a1376d97
benlu <benlu@benlutekiMacBook-Pro.local>
2014-02-06 17:07:04 +0800

    Initial Commit

---
+++ coredata/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  coredata
+//
+//  Created by benlu on 2/6/14.
+//  Copyright (c) 2014 benlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ coredataTests/coredataTests.m
@@ -0,0 +1,34 @@
+//
+//  coredataTests.m
+//  coredataTests
+//
+//  Created by benlu on 2/6/14.
+//  Copyright (c) 2014 benlu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface coredataTests : XCTestCase
+
+@end
+
+@implementation coredataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
